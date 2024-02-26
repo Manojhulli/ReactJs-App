534a8ad27d1b1b59837e108cf18e98c6ec23d5f1
ManojHulli <0.manojhulli@gmail.com>
2024-02-26 19:38:15 +0530

    clear button

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -13,22 +13,29 @@ export default function TextArea(props) {
   const handleText = (event) =>{
     setText (event.target.value);
   }
-  const  [text , setText] = useState("enter text here : "); 
+
+  const handleclear =() => {
+    let newText = "";
+    setText(newText);
+  }
+  const  [text , setText] = useState(" "); 
   return (
     <>
     <div>
         <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleText} id="myBox" rows="8"></textarea>
+        <div className="mb-3 ">
+          
+            <textarea className="form-control" value={text} onChange={handleText} id="myBox" rows="10"></textarea>
         </div>
         <>
-            <button type="button" class="btn btn-primary mx-1" onClick={handleUppercase}>Convert to upper case</button>
-            <button type="button" class="btn btn-primary mx-2" onClick={handleLowercase}>Convert to Lower case</button>
+            <button type="button" class="btn btn-dark mx-1" onClick={handleUppercase}>Convert to upper case</button>
+            <button type="button" class="btn btn-dark mx-2" onClick={handleLowercase}>Convert to Lower case</button>
+            <button type="button" class="btn btn-dark mx-2" onClick={handleclear}>Clear</button>
             </>
     </div>
 
-    <div className="container mb-2">
-      <h1>Your Text Summary</h1>
+    <div className="container mb-2 ">
+      <h3>Your Text Summary</h3>
       <p> {text.split(" ").length} words and {text.length}Characters</p>
     </div>
     </>
